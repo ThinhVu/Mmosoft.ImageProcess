e57038bf5fc477e72a1c70492165a75febcd87c1
ThinhVu <vutrongthinhk7@gmail.com>
2019-04-20 11:55:43 +0700

    Add 3D effect

---
+++ src/Effects/ThreeD.cs
@@ -0,0 +1,37 @@
+﻿namespace ImageProcess.Effects
+{
+    public class ThreeD : IImageEffect
+    {
+        public void Apply(Pixel[,] pxs)
+        {
+            int offset = 3;
+
+            Pixel[,] pxsc = new Pixel[pxs.GetLength(0), pxs.GetLength(1)];
+
+            for (int i = 0, iMax = pxs.GetLength(0); i < iMax; i++)
+            {
+                for (int j = 0, jMax = pxs.GetLength(1); j < jMax; j++)
+                {
+                    pxsc[i, j] = pxs[i, j];
+                }
+            }
+
+            for (int i = offset, iMax = pxs.GetLength(0) - offset; i < iMax; i++)
+            {
+                for (int j = 0, jMax = pxs.GetLength(1); j < jMax; j++)
+                {
+                    pxsc[i, j].G = pxs[i + offset, j].G;
+                    pxsc[i, j].R = pxs[i - offset, j].R;
+                }
+            }
+
+            for (int i = 0, iMax = pxs.GetLength(0); i < iMax; i++)
+            {
+                for (int j = 0, jMax = pxs.GetLength(1); j < jMax; j++)
+                {
+                    pxs[i, j] = pxsc[i, j];
+                }
+            }
+        }
+    }
+}
